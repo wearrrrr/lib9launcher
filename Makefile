2857a617ac9000e0abff1a31fecd1b0224144a1f
wearrrrr <contact@wearr.dev>
2023-11-30 20:59:19 -0600

    wow the code is actually good now

--- Makefile
+++ Makefile
@@ -1,17 +1,32 @@
 # lib9launcher Makefile
 
-	CC = i686-w64-mingw32-gcc
+	CC = clang
 	DLL_NAME = lib9launcher.dll
 	INPUT_FILES = main.cpp
 	WIN32UTF8 = ../thcrap/libs/win32_utf8
 	THCRAP_EXTERNAL_DEPS = ../thcrap/libs/external_deps/include/
 	THCRAP_HEADERS = ../thcrap/thcrap/src
 
+	RELEASE = TRUE
+
 build:
-	${CC} -shared -o ${DLL_NAME} ${INPUT_FILES} thcrap.lib -I${WIN32UTF8} -I${THCRAP_EXTERNAL_DEPS} -I${THCRAP_HEADERS}
+	${CC} -target i686-pc-win32-gnu -shared ${INPUT_FILES} -Oz thcrap.lib -I${WIN32UTF8} -I${THCRAP_EXTERNAL_DEPS} -I${THCRAP_HEADERS} -D_GLIBCXX_USE_DEPRECATED=0 -nostdlib -o ${DLL_NAME}
+	@if [ -f ${DLL_NAME} ]; then \
+		echo "Build successful!"; \
+	else \
+		echo "Build failed!"; \
+	fi
+
+strip:
+	@echo "Stripping symbols..."
+	@strip ${DLL_NAME}
+	@echo "Done!"
 
 clean:
 	@echo "Cleaning up..."
 	@rm -f ${DLL_NAME}
 	@echo "Done!"
-	make build
+
+.PHONY: clean build strip
+
+all: clean build strip
--- main.cpp
+++ main.cpp
@@ -1,7 +1,71 @@
 #include <thcrap.h>
 #include <windows.h>
+#include <stdint.h>
+#include "main.hpp"
 
-void th06_mem_scan()
+#define WIN32_LEAN_AND_MEAN
+
+#undef IN
+
+struct GameDataOffsets {
+  uintptr_t lives;
+  uintptr_t bombs;
+  uintptr_t power;
+  uintptr_t character;
+  uintptr_t shottype;
+  uintptr_t score;
+  uintptr_t stage;
+  uintptr_t difficulty;
+  uintptr_t gamestate;
+  uint8_t game;
+};
+
+enum GameId : uint8_t {
+  EoSD,
+  PCB,
+  IN,
+  PoFV,
+  StB,
+  MoF,
+  SA,
+  UFO,
+  DS,
+  GFW,
+  TD,
+  DDC,
+  ISC,
+  LoLK,
+  HSiFS,
+  VD,
+  WBaWC,
+  UM,
+  HBM,
+  UDoALG,
+  GAME_UNKNOWN
+};
+
+GameId GAME_ID;
+
+static const GameDataOffsets game_offsets_lookup[] {
+    // Currently only EoSD is supported, I had to spend all day refactoring the code give me a break D:
+  [EoSD] = {
+    .lives = TH06_LIVES_ADDR,
+    .bombs = TH06_BOMBS_ADDR,
+    .power = TH06_POWER_ADDR,
+    .character = TH06_CHAR_ADDR,
+    .shottype = TH06_SHOTTYPE_ADDR,
+    .score = TH06_SCORE_ADDR,
+    .stage = TH06_STAGE_ADDR,
+    .difficulty = TH06_DIFFICULTY_ADDR,
+    .gamestate = TH06_GAMESTATE_ADDR,
+    .game = EoSD
+    },
+  };
+
+const GameDataOffsets* current_game_offsets;
+
+
+DWORD __stdcall mem_scan(void*)
 {
     while (true)
     {
@@ -9,31 +73,52 @@ void th06_mem_scan()
         if (hFile == INVALID_HANDLE_VALUE)
         {
             log_print("[9L] Failed to create file!\n");
-            return;
+            return 1;
         }
         // Write the content of the buffer to the file
         DWORD bytesWritten;
-        int8_t lives = *(char *)0x0069d4ba;
-        int8_t bombs = *(char *)0x0069d4bb;
-        unsigned short power = *(unsigned short *)0x0069d4b0;
-        int8_t character = *(char *)0x0069d4bd;
-        int8_t shottype = *(char *)0x0069d4be;
-        int score = *(int *)0x0069bca4;
-        int stage = *(int *)0x0069d6d4;
-        int difficulty = *(int *)0x0069bcb0;
-        int gamestate = *(int *)0x006c6ea0;
-
+        
+        // Get the game data from game_offsets_lookup
+        current_game_offsets = &game_offsets_lookup[GAME_ID];
+    
         // Write information to JSON file, make sure its formatted with tabs
         // TODO: make this a bit more elegant, maybe use a library
-        const char *json = "{\n\t\"lives\": %d,\n\t\"bombs\": %d,\n\t\"power\": %d,\n\t\"character\": %d,\n\t\"shottype\": %d,\n\t\"score\": %d,\n\t\"stage\": %d,\n\t\"difficulty\": %d,\n\t\"gamestate\": %d\n}";
+        const char *json = "{\n\t\"lives\": %d,\n\t\"bombs\": %d,\n\t\"power\": %d,\n\t\"character\": %d,\n\t\"shottype\": %d,\n\t\"score\": %d,\n\t\"stage\": %d,\n\t\"difficulty\": %d,\n\t\"gamestate\": %d,\n\t\"game\": %d\n}";
         char *buffer = (char *)malloc(strlen(json) + 4);
-        sprintf(buffer, json, lives, bombs, power, character, shottype, score, stage, difficulty, gamestate);
+        sprintf(buffer, json, *(uint8_t *)current_game_offsets->lives, *(uint8_t *)current_game_offsets->bombs, *(uint8_t *)current_game_offsets->power, *(uint8_t *)current_game_offsets->character, *(uint8_t *)current_game_offsets->shottype, *(uint32_t *)current_game_offsets->score, *(uint8_t *)current_game_offsets->stage, *(uint8_t *)current_game_offsets->difficulty, *(uint8_t *)current_game_offsets->gamestate, (uint8_t)current_game_offsets->game);
         WriteFile(hFile, buffer, strlen(buffer), &bytesWritten, NULL);
 
         CloseHandle(hFile);
 
+        free(buffer);
+
         Sleep(300);
     }
+}
+
+#define CHECK_SLUG(name, val) if (strcmp(gameSlug, name) == 0) return val;
+GameId determineGameIDFromSlug(const char* gameSlug) {
+    CHECK_SLUG("th06", EoSD);
+    CHECK_SLUG("th07", PCB);
+    CHECK_SLUG("th08", IN);
+    CHECK_SLUG("th09", PoFV);
+    CHECK_SLUG("th095", StB);
+    CHECK_SLUG("th10", MoF);
+    CHECK_SLUG("th11", SA);
+    CHECK_SLUG("th12", UFO);
+    CHECK_SLUG("th125", DS);
+    CHECK_SLUG("th128", GFW);
+    CHECK_SLUG("th13", TD);
+    CHECK_SLUG("th14", DDC);
+    CHECK_SLUG("th143", ISC);
+    CHECK_SLUG("th15", LoLK);
+    CHECK_SLUG("th16", HSiFS);
+    CHECK_SLUG("th165", VD);
+    CHECK_SLUG("th17", WBaWC);
+    CHECK_SLUG("th18", UM);
+    CHECK_SLUG("th185", HBM);
+    CHECK_SLUG("th19", UDoALG);
+    return GAME_UNKNOWN;
 }
 
 extern "C"
@@ -64,11 +149,10 @@ extern "C"
 
         CloseHandle(hFile);
 
-        const char *game = runconfig_game_get();
-        log_printf("[9L] Game: %s\n", game);
-        if (strcmp(game, "th06") == 0) {
-            HANDLE hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)th06_mem_scan, NULL, 0, NULL);
-        }
+        // Get the game ID from the runconfig
+        GAME_ID = determineGameIDFromSlug(runconfig_game_get());
+        // Start the memory scanning thread :D
+        HANDLE hThread = CreateThread(NULL, 0, mem_scan, NULL, 0, NULL);
 
         return 0;
     }
